9c8536b3b334a538bf7248e6939259e4e7f62397
cnmz <cnmz@msn.com>
2021-02-01 19:10:16 +0800

    one lesson

--- demo.ts
+++ demo.ts
@@ -5,3 +5,16 @@ const teacher: Person = {
 }
 
 console.log(teacher);
+
+
+// 基本类型 number string boolean symbol null undfined void
+// 对象类型 {} [] function class
+
+// 类型注解 type annotation  定义变量 带类型
+// 类型推断 type inference   TS自动推断中变量类型
+
+// 能推断就推断，无法推断使用类型注解
+
+
+
+
